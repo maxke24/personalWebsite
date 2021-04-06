233dc99080199909aebec052894ddc343a9eb1de
master <jellemax08@hotmail.com>
2021-04-06 23:41:50 +0200

    changing nav bar

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,10 +9,10 @@ let options = [
     /*3*/["#2c2b3cff", "#403f4cff", "#ffffffff", "#121420ff", "#1b2432ff", "#F6E8EAff", "#F6E8EAff", "#b76d68"],
     /*4*/["#89ABE3FF", "#FCF6F5FF", "#20202080", "#FCF6F5FF", "#FCF6F5FF", "#20202080", "#20202080", "#b76d68"],
     /*5*/["#363333ff", "#272121ff", "#ffffffff", "#f6e9e9ff", "#f6e9e991", "#270F36ff", "#270F36ff", "#e16428"],
-    /*6*/["#dbedf3ff", "#404b69ff", "#ffffffff", "#404b69ff", "#283149ff", "#ffffffff", "#ffffffff", "#e16428ff"],
+    /*6*/["#dbedf3ff", "#404b69ff", "#20202080", "#404b69ff", "#283149ff", "#ffffffff", "#ffffffff", "#e16428ff"],
     /*7*/["#203647ff", "#007CC7ff", "#ffffffff", "#007CC7ff", "#4DA8DAff", "#EEFBFBff", "#EEFBFBff", "#12232E"],
     /*8*/["#202020ff", "#3F3F3Fff", "#ffffffff", "#202020ff", "#3F3F3Fff", "#ffffffff", "#707070ff", "#FFDf6C"],
-    /*9*/["#333D79FF", "#FAEBEFFF", "#20202080", "#FAEBEFFF", "#FAEBEFFF", "#20202080", "#20202080", "#b76d68"],
+    /*9*/["#333D79FF", "#FAEBEFFF", "#ffffffff", "#FAEBEFFF", "#FAEBEFFF", "#20202080", "#20202080", "#b76d68"],
     /*10*/]
 
 function openNav() {
