346f670338018045aabf27edbc4b9a80f1ac297b
jelle <jellemax08@hotmail.com>
2022-05-17 17:26:54 +0200

    Adding a blog and removing the pink

--- assets/js/index.js
+++ assets/js/index.js
@@ -21,6 +21,7 @@ function init() {
 	const experiencesSection = document.querySelector(".experiences>div");
 	const projectsSection = document.querySelector(".projects>div");
 	const eventsSection = document.querySelector(".events>div");
+	const blogsSection = document.querySelector(".blogs>div");
 
 	fetch("../../experiences.json")
 	.then(response => {
@@ -29,29 +30,17 @@ function init() {
 		const experiences = output.Experiences;
 		const projects = output.projects;
 		const events = output.events;
+		const blogs = output.blogs;
 		fillSection(experiencesSection, experiences);
 		fillSection(projectsSection, projects);
 		fillSection(eventsSection, events);
+		fillBlogSection(blogsSection, blogs);
 	});
 }
 
 function fillSection(section, data){
 	for(const o in data){
 		const el = data[o];
-
-/* 		const art = `<article class="${el.Class}">
-						<div class="blur">
-						<h4>${el.Location}</h4></div>
-						<div class="overlay">
-							<p>${el.Description}</p>
-						</div>
-					</article>`; */
-/* 		const art = `<a href="#" class="control_next">></a>
-		<a href="#" class="control_prev"><</a>
-		<ul>
-		  <li>SLIDE 1</li>
-		</ul> `; */
-
 		const art = `<div id='container' class="${el.Class}">
 						<div id='text'>
 							<h4>${el.Location}</h4>
@@ -61,6 +50,23 @@ function fillSection(section, data){
 							<img alt='job' src="${el.Link}" />
 					</div>
 					<a id="next">Next</a>
+					</div>`;
+
+		section.innerHTML += art;
+	}
+}
+
+function fillBlogSection(section, data){
+	for(const o in data){
+		const el = data[o];
+		let tag = "";
+		if(el.Link){
+			tag = `<a class="blogLink" href='${el.Link}'>Bap - click here!</a>`;
+		}
+		console.log(el.Link);
+		const art = `<div id='container' class="${el.Class}">
+							<h4>${el.Location}</h4>
+						<p>${el.Description} ${tag}</p>
 					</div>`;
 
 		section.innerHTML += art;
