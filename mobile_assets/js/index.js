b267313c01ef5d3af48a48e1cb7e78a2b997069c
Jelle Max <jelle.max@hotmail.com>
2023-04-08 02:23:53 +0200

    updates

--- mobile_assets/js/index.js
+++ mobile_assets/js/index.js
@@ -1,29 +1,36 @@
-fetch('../../assets/experiences_new.json')
-    .then((response) => response.json())
-    .then((data) => {
+fetch("../../assets/experiences_new.json")
+  .then((response) => response.json())
+  .then((data) => {
+    for (let key in data) {
+      let cont = document.querySelector(
+        `.${key} .carousel-container .carousel`
+      );
+      if (cont) {
+        for (let item in data[key]) {
+          let title = data[key][item].Title;
+          let description = data[key][item].Description;
 
-        for (let key in data) {
-            let cont = document.querySelector(`.${key} .carousel-container .carousel`)
-            if(cont){
-                for (let item in data[key]) {
-                    let title = data[key][item].Title;
-                    let description = data[key][item].Description;
-                
-                        cont.innerHTML += `<div class="card">
+          cont.innerHTML += `<div class="card">
                         <div class="card-body">
                         <h3 class="card-title">${title}</h3>
                         <p class="card-text">${description}</p>
                         </div>
-                        </div>`
-                    
-                }
-            }else if(key == "About"){
-                let cont = document.querySelector(`.${key}`)
-                for (let item in data[key]) {
-                    let description = data[key][item].Description;
-                cont.innerHTML += `<div class="aboutMe">
+                        </div>`;
+        }
+      } else if (key == "About") {
+        let cont = document.querySelector(`.${key}`);
+        for (let item in data[key]) {
+          let description = data[key][item].Description;
+          cont.innerHTML += `<div class="aboutMe">
                 <p>${description}</p>
-                </div>`
-            }}
-          }
+                </div>`;
+        }
+      }
+    }
+    document.querySelectorAll(".card").forEach((el) => {
+      el.addEventListener("click", () => {
+        document.querySelector(".card-blur").classList.toggle("show");
+        el.classList.toggle("active");
+      });
     });
+  });
